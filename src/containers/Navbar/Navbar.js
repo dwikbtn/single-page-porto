cff8563f18604be780a7d23cd0f05b2096763f81
Dwi Darma <dwikbtn15@gmail.com>
2021-04-19 13:24:57 +0800

    finish the navbar

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Logo from "../../components/navbar/companyLogo/logo";
 import Navlist from "../../components/navbar/navlist/navlist";
+
+import styles from "./navbar.module.scss";
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <Logo />
-        <Navlist />
+        <nav className={styles.navbar}>
+          <Logo />
+          <Navlist />
+        </nav>
       </div>
     );
   }
